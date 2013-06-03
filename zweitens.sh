375aa6cd14918dbbc743ea35d1f9dda9aa131f94
xabgesagtx <oliver.behncke@haw-hamburg.de>
2013-06-03 13:52:33 +0300

    Create zweitens.sh

---
+++ zweitens.sh
@@ -0,0 +1,14 @@
+1)
+# set up
+sudo iptables -I INPUT -s 172.16.1.0/24 -j DROP
+sudo iptables -I OUTPUT -s 172.16.1.0/24 -j DROP
+
+# clean up
+sudo iptables -D OUTPUT -s 172.16.1.0/24 -j DROP
+sudo iptables -D OUTPUT -s 172.16.1.0/24 -j DROP
+
+
+
+# 2)
+sudo iptables -I INPUT -s 172.16.1.0/24 -p tcp --dport 50000 -j ACCEPT
+sudo iptables -I OUTPUT -s 172.16.1.0/24 -p tcp --sport 50000 -j ACCEPT
