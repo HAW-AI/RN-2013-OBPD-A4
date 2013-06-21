69769fedad099f49b6cd1faded8220e08cf33f8a
xabgesagtx <oliver.behncke@haw-hamburg.de>
2013-06-21 11:54:57 +0200

    Update zweitens.sh

--- zweitens.sh
+++ zweitens.sh
@@ -12,3 +12,9 @@ sudo iptables -D OUTPUT -s 172.16.1.0/24 -j DROP
 # 2)
 sudo iptables -I INPUT -s 172.16.1.0/24 -p tcp --dport 50000 -j ACCEPT
 sudo iptables -I OUTPUT -s 172.16.1.0/24 -p tcp --sport 50000 -j ACCEPT
+
+
+# 3)
+sudo /usr/sbin/iptables -I OUTPUT -p icmp -m state --state NEW -j ACCEPT
+sudo /usr/sbin/iptables -I INPUT -p icmp -m state --state ESTABLISHED -j ACCEPT
+sudo /usr/sbin/iptables -I INPUT -p icmp -m state --state NEW -j DROP
